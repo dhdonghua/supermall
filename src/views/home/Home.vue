bfc8f408f80d55ab61b677d9013a1c07bdcdcca0
dhdonghua <291938276@qq.com>
2021-09-06 18:01:59 +0800

    新增首页顶部导航栏navbar；新增axios和相关配置；配置跨域代理；

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>首页</div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template #center>首页</template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home.js";
+
 export default {
-  name: 'Home'
-}
+  name: "Home",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      homedata: [],
+    };
+  },
+  created() {
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      //this.homedata = res;
+    });
+  },
+};
 </script>
 
 <style>
-
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
