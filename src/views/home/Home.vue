a09f3d2eeb223a53e47105a3fd46956a468d31f5
dhdonghua <291938276@qq.com>
2021-09-07 16:35:42 +0800

    新增首页的轮播图组件swiper

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav">
-      <template #center>首页</template>
+      <template #center>购物街</template>
     </nav-bar>
+    <home-swiper :banners="banners" />
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper";
+
 import { getHomeMultidata } from "network/home.js";
 
 export default {
   name: "Home",
   components: {
     NavBar,
+    HomeSwiper
   },
   data() {
     return {
-      homedata: [],
+      banners: [],
+      recommend: [],
     };
   },
   created() {
     getHomeMultidata().then((res) => {
-      console.log(res);
-      //this.homedata = res;
+      this.banners = res.data.banner.list;
+      this.recommend = res.data.recommend.list;
     });
   },
 };
@@ -33,5 +38,8 @@ export default {
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+}
+img {
+  width: 100%;
 }
 </style>
